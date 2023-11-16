a9d2131fe2faf52334a173dcb288e025f187340c
Julius <julius.emil.petersson@gmail.com>
2023-11-16 16:08:52 +0100

    - styling - css var - nodejs stöd för css och ttffiler

--- app.js
+++ app.js
@@ -11,10 +11,8 @@ const hotModuleConfig = require("webpack-hot-middleware")
 const compiler = webpack(config);
 
 const server = http.createServer((req, res) => {
-    // Determine the requested file path
     const filePath = path.join(__dirname, req.url);
 
-    // Serve the index.html file by default
     if (req.url === '/') {
         fs.readFile(path.join(__dirname, '/index.html'), (err, data) => {
             if (err) {
@@ -26,7 +24,6 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
     }
-    // Serve JavaScript modules with the correct MIME type
     else if (path.extname(filePath) === '.js') {
         fs.readFile(filePath, (err, data) => {
             if (err) {
@@ -38,18 +35,35 @@ const server = http.createServer((req, res) => {
             res.end(data);
         });
     }
-    // Handle other file types as needed
+    else if (path.extname(filePath) === '.css') {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.end(data);
+        });
+    }
+    else if (path.extname(filePath) === '.ttf') {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'font/ttf' });
+            res.end(data);
+        });
+    }
     else {
-        // Handle other file types or routes here
-        // For example, you can serve CSS, images, or other resources.
         res.writeHead(404);
         res.end('Not Found');
     }
 });
 
-// Use Webpack Dev Middleware
 server.on('request', (req, res) => {
-    // Use the middleware to handle requests
     hotModuleConfig(compiler);
 
     webpackDevMiddleware(compiler, {
